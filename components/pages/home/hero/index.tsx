576d3b6d4df7267454b1f4a7c4c155b34bfe8c68
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 13:24:08 -0600

    feat(home): setup hero component

--- components/pages/home/hero/index.tsx
+++ components/pages/home/hero/index.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+
 const Hero = () => {
   return (
-    <div>
-      <p>Hero Component</p>
-    </div>
+    <main className='h-[calc(100vh-1rem-3.5rem)] px-4'>
+      <div className='flex h-full flex-col justify-center'>
+        <h1 className='text-5xl font-bold'>Weebn</h1>
+        <h2 className='mb-4 text-xl text-zinc-700'>
+          Weeb widgets for Notion, free and easy to use
+        </h2>
+        <Link className='primary-button inline-block' href='#widgets'>
+          Explore Widgets
+        </Link>
+      </div>
+    </main>
   );
 };
 
