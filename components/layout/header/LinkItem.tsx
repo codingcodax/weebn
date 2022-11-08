52588cd3dfd750539eea8174d1aa2e83964afffa
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 10:33:41 -0600

    feat(header): setup link item component

--- components/layout/header/LinkItem.tsx
+++ components/layout/header/LinkItem.tsx
@@ -1,8 +1,16 @@
-const LinkItem = () => {
+import Link from 'next/link';
+import { FC, ReactNode } from 'react';
+
+interface LinkItemProps {
+  href: string;
+  children: ReactNode;
+}
+
+const LinkItem: FC<LinkItemProps> = ({ href, children }) => {
   return (
-    <div>
-      <p>Link Item Component</p>
-    </div>
+    <li className='text-zinc-500 transition-colors duration-200 hover:text-zinc-600'>
+      <Link href={href}>{children}</Link>
+    </li>
   );
 };
 
