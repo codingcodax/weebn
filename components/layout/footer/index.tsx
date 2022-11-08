6ff92cae9e553592599db6fb4bb2e7530bfe1f03
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 10:26:14 -0600

    fix(layout): update padding from y to x

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -4,7 +4,7 @@ import Links from './Links';
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col items-center space-y-16 py-4 pb-8'>
+    <footer className='flex flex-col items-center space-y-16 px-4 pb-8'>
       <Links />
       <p>
         &copy; 2022 — Handcrafted with 💜 by{' '}
