0cf39581f5e4f39449cf544422a40503bbabeab7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 16:31:06 -0600

    feat(widgets): setup widgets list component

--- components/pages/home/explore-widgets/WidgetsList.tsx
+++ components/pages/home/explore-widgets/WidgetsList.tsx
@@ -1,8 +1,35 @@
+import WidgetItem from './WidgetItem';
+
+const widgetsItems = [
+  {
+    href: '/quotes',
+    title: 'Quotes',
+    description: 'Anime quotes from AnimeChan',
+  },
+  {
+    href: '/image-galley',
+    title: 'Image Gallery',
+    description: 'Images from Nekos',
+  },
+  {
+    href: 'daily-anime',
+    title: 'Random Anime',
+    description: 'Show a random anime to see',
+  },
+];
+
 const WidgetsList = () => {
   return (
-    <div>
-      <p>Widgets List Component</p>
-    </div>
+    <ul className='grid grid-cols-1 gap-4 min-[500px]:grid-cols-2'>
+      {widgetsItems.map(({ href, title, description }) => (
+        <WidgetItem
+          key={title}
+          description={description}
+          href={href}
+          title={title}
+        />
+      ))}
+    </ul>
   );
 };
 
