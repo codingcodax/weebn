1d368d41cf33db3fb9ecb85b9f6dc006b71eaa8a
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 16:30:47 -0600

    feat(widgets): setup widget item component

--- components/pages/home/explore-widgets/WidgetItem.tsx
+++ components/pages/home/explore-widgets/WidgetItem.tsx
@@ -1,8 +1,22 @@
-const WidgetItem = () => {
+import Link from 'next/link';
+import { FC } from 'react';
+
+interface WidgetItemProps {
+  href: string;
+  title: string;
+  description: string;
+}
+
+const WidgetItem: FC<WidgetItemProps> = ({ href, title, description }) => {
   return (
-    <div>
-      <p>Widget Item Component</p>
-    </div>
+    <li className='rounded-md border border-zinc-900'>
+      <div className='aspect-video rounded-md rounded-b-xl bg-green-200'></div>
+
+      <Link className='inline-block py-4 px-2' href={`/editor/${href}`}>
+        <h4 className='text-lg'>{title}</h4>
+        <p className='text-zinc-700'>{description}</p>
+      </Link>
+    </li>
   );
 };
 
