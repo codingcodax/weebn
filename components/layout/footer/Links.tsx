36c7b5d67c39d7385a457ebd4a5d3250d635cacb
Alexis Guzman <alexis12989x@gmail.com>
2022-11-08 10:23:35 -0600

    feat(footer): setup links component

--- components/layout/footer/Links.tsx
+++ components/layout/footer/Links.tsx
@@ -1,8 +1,12 @@
+import LinkItem from './LinkItem';
+
 const Links = () => {
   return (
-    <div>
-      <p>Links Component</p>
-    </div>
+    <ul className='flex space-x-8'>
+      <LinkItem href='/usage'>Usage Tutorial</LinkItem>
+      <LinkItem href='/contribute'>Contribute</LinkItem>
+      <LinkItem href='/relase-logs'>Release Logs</LinkItem>
+    </ul>
   );
 };
 
