d934398d5ad693b54b8644c8fc6463d8cdb62ee7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-09 18:13:17 -0600

    feat(styles): add max width and margin auto styles

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -2,7 +2,7 @@ import Links from './Links';
 
 const Header = () => {
   return (
-    <header className='flex justify-between px-4 pt-8'>
+    <header className='mx-auto flex w-full max-w-6xl justify-between px-4 pt-8'>
       <p>Logo</p>
       <Links />
     </header>
