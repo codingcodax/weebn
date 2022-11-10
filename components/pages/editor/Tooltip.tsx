81454a864b47406dbfe9ff20a7e930d471ecb912
Alexis Guzman <alexis12989x@gmail.com>
2022-11-10 17:35:34 -0600

    feat(editor): setup tooltip component

--- components/pages/editor/Tooltip.tsx
+++ components/pages/editor/Tooltip.tsx
@@ -1,7 +1,10 @@
 const Tooltip = () => {
   return (
-    <div>
-      <p>Tooltip Component</p>
+    <div className='rouded-md bg-zinc-100 px-6 py-4'>
+      <p>
+        <span className='font-mono'>💡</span> Drag the corner rigth at the
+        bottom to preview the widget in different sizes
+      </p>
     </div>
   );
 };
