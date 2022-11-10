ad4db4a7289f04fd5ce655b3eadc0aef861d4f33
Alexis Guzman <alexis12989x@gmail.com>
2022-11-10 13:14:31 -0600

    feat(app): update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import { Footer, Header } from '~/components/layout';
-
 import '~/styles/globals.css';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -10,11 +8,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='grid h-screen grid-rows-[auto_1fr_auto] gap-y-4'>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
