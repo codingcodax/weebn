464be23cb0a687f0e0b8a16e2bd4c93b3bfa44ad
Alexis Guzman <alexis12989x@gmail.com>
2022-11-10 16:25:08 -0600

    feat(ui): setup divider component

--- components/ui/Divider.tsx
+++ components/ui/Divider.tsx
@@ -1,9 +1,12 @@
-const Divider = () => {
-  return (
-    <div>
-      <p>Divider Component</p>
-    </div>
-  );
+import { FC } from 'react';
+import * as Separator from '@radix-ui/react-separator';
+
+interface DividerProps {
+  orientation?: 'horizontal' | 'vertical';
+}
+
+const Divider: FC<DividerProps> = ({ orientation }) => {
+  return <Separator.Root orientation={orientation} />;
 };
 
 export default Divider;
