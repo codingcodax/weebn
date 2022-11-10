70bb7f8b8844592548f7e014f6792802608107b7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-10 17:20:34 -0600

    feat(editor): setup live preview component

--- components/pages/editor/LivePreview.tsx
+++ components/pages/editor/LivePreview.tsx
@@ -1,7 +1,13 @@
-const LivePreview = () => {
+import { FC } from 'react';
+
+interface LivePreviewProps {
+  page: string;
+}
+
+const LivePreview: FC<LivePreviewProps> = ({ page }) => {
   return (
-    <div>
-      <p>Live Preview Component</p>
+    <div className='h-[250px] w-[600px] resize overflow-auto rounded-md border-2 border-dashed border-zinc-500'>
+      <iframe height='100%' src={`http://localhost:3000${page}`} width='100%' />
     </div>
   );
 };
