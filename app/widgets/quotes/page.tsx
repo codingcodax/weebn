e2b49d650a68b36dac5696c39c832a48e9d8ee85
Alexis Guzman <alexis12989x@gmail.com>
2022-11-10 16:24:51 -0600

    feat(widgets): setup quotes page

--- app/widgets/quotes/page.tsx
+++ app/widgets/quotes/page.tsx
@@ -1,7 +1,17 @@
 const Quotes = () => {
   return (
-    <div>
-      <p>Quotes Page</p>
+    <div className='flex h-screen flex-col items-center justify-center p-4'>
+      <div className='max-w-lg space-y-6'>
+        <h1 className='text-xl italic'>
+          &quot;Listen to yourself whining and complaining like some sorry
+          little victim! You can whimper all day for all I care, you&apos;re
+          nothing but a coward!&quot;
+        </h1>
+
+        <div className='h-px w-1/2 bg-zinc-500' />
+
+        <h2>Naruto Uzumaki &mdash; Naruto</h2>
+      </div>{' '}
     </div>
   );
 };
