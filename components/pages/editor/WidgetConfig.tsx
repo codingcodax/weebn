1e5ff7095d20fd67d9e2ad8e0f32ac5869f80768
Alexis Guzman <alexis12989x@gmail.com>
2022-11-11 14:38:49 -0600

    feat(editor): setup widget config component

--- components/pages/editor/WidgetConfig.tsx
+++ components/pages/editor/WidgetConfig.tsx
@@ -1,7 +1,62 @@
-const WidgetConfig = () => {
+import Link from 'next/link';
+
+import { Icons, Input } from '~/components/ui';
+
+interface WidgetConfigProps {
+  children: React.ReactNode;
+}
+
+const WidgetConfig = ({ children }: WidgetConfigProps) => {
+  return (
+    <div className='grid h-screen grid-rows-[auto_auto_1fr_auto] gap-y-8 bg-zinc-100 px-4 py-8'>
+      {children}
+    </div>
+  );
+};
+
+// eslint-disable-next-line react/display-name
+WidgetConfig.Nav = () => {
+  return (
+    <header>
+      <Link href='/'>Logo</Link>
+    </header>
+  );
+};
+
+interface WidgetConfigTitleProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+WidgetConfig.Title = ({ children }: WidgetConfigTitleProps) => {
+  return (
+    <nav>
+      <Link className='flex items-center space-x-4 text-lg' href='/'>
+        <Icons.chevronLeft />
+        <h2>{children}</h2>
+      </Link>
+    </nav>
+  );
+};
+
+interface WidgetConfigFooterProps {
+  widgetLink: string;
+}
+
+// eslint-disable-next-line react/display-name
+WidgetConfig.Footer = ({ widgetLink }: WidgetConfigFooterProps) => {
   return (
     <div>
-      <p>Widgets Config Component</p>
+      <div className='h-px bg-zinc-500' />
+
+      <p className='mt-8 mb-4 text-zinc-800'>
+        Copy Link (then paste into Notion, and click &quot;Embed&quot;)
+      </p>
+
+      <div className='relative'>
+        <Input disabled id='widgetLink' value={widgetLink} />
+        <Icons.copy className='absolute top-3 right-4' height='16' width='16' />
+      </div>
     </div>
   );
 };
