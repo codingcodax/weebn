125292749ecb8f8a685200ab1141bb5f1c44ee2e
Alexis Guzman <alexis12989x@gmail.com>
2022-11-11 18:08:01 -0600

    feat(editor): setup settings pane component

--- components/pages/editor/SettingsPane.tsx
+++ components/pages/editor/SettingsPane.tsx
@@ -1,9 +1,41 @@
-const SettingsPane = () => {
-  return (
-    <div>
-      <p>Settings Pane Component</p>
-    </div>
-  );
+interface SettingsPaneProps {
+  children: React.ReactNode;
+}
+
+const SettingsPane = ({ children }: SettingsPaneProps) => {
+  return <div className='space-y-8'>{children}</div>;
+};
+
+interface SettingsPaneSectionProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+SettingsPane.Section = ({ children }: SettingsPaneSectionProps) => {
+  return <div className='space-y-6'>{children}</div>;
+};
+
+// eslint-disable-next-line react/display-name
+SettingsPane.Divider = () => {
+  return <div className='h-px bg-zinc-500' />;
+};
+
+interface SettingsPaneTitleProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+SettingsPane.Title = ({ children }: SettingsPaneTitleProps) => {
+  return <p className='text-sm uppercase text-zinc-500'>{children}</p>;
+};
+
+interface SettingsPaneOptionProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+SettingsPane.Option = ({ children }: SettingsPaneOptionProps) => {
+  return <div>{children}</div>;
 };
 
 export default SettingsPane;
