23b2a5066a129632c1aa6c549f73a0418b817fbb
Alexis Guzman <alexis12989x@gmail.com>
2022-11-11 18:07:43 -0600

    feat(editor): export option item component

--- components/pages/editor/index.ts
+++ components/pages/editor/index.ts
@@ -1,4 +1,5 @@
 export { default as LivePreview } from './LivePreview';
+export { default as OptionItem } from './OptionItem';
 export { default as SettingsPane } from './SettingsPane';
 export { default as Tooltip } from './Tooltip';
 export { default as WidgetConfig } from './WidgetConfig';
