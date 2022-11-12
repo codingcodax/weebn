839d8f59c108c5791a94b3ceabf8d2c4441b08cf
Alexis Guzman <alexis12989x@gmail.com>
2022-11-11 18:09:54 -0600

    feat(editor): setup option item component

--- components/pages/editor/OptionItem.tsx
+++ components/pages/editor/OptionItem.tsx
@@ -1,7 +1,44 @@
-const OptionItem = () => {
+import { Switch } from '~/components/ui';
+
+interface OptionItemProps {
+  children: React.ReactNode;
+}
+
+const OptionItem = ({ children }: OptionItemProps) => {
+  return <label className='grid grid-cols-[1fr_auto]'>{children}</label>;
+};
+
+interface OptionItemNameProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+OptionItem.Name = ({ children }: OptionItemNameProps) => {
+  return <div>{children}</div>;
+};
+
+interface OptionItemDescriptionProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+OptionItem.Description = ({ children }: OptionItemDescriptionProps) => {
+  return <p className='row-start-2 text-sm text-zinc-600'>{children}</p>;
+};
+
+interface OptionItemSwitchProps {
+  enabled?: boolean;
+  setEnabled: () => void;
+}
+
+// eslint-disable-next-line react/display-name
+OptionItem.Switch = ({
+  enabled = false,
+  setEnabled,
+}: OptionItemSwitchProps) => {
   return (
-    <div>
-      <p>Option Item Component</p>
+    <div className='col-start-2 row-span-2 grid items-center'>
+      <Switch enabled={enabled} setEnabled={setEnabled} />
     </div>
   );
 };
