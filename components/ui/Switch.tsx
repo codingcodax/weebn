acb59754aeab1b3999e7c72da395e33952779c4c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-12 09:07:20 -0600

    feat(ui): update props for switch component

--- components/ui/Switch.tsx
+++ components/ui/Switch.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import { Switch } from '@headlessui/react';
-import { FC } from 'react';
+import { Dispatch, FC } from 'react';
 
 interface MySwitchProps {
-  enabled: boolean;
-  setEnabled: () => void;
+  isEnabled: boolean;
+  toggle: Dispatch<boolean>;
 }
 
-const MySwitch: FC<MySwitchProps> = ({ enabled, setEnabled }) => {
+const MySwitch: FC<MySwitchProps> = ({ isEnabled, toggle }) => {
   return (
     <Switch
-      checked={enabled}
+      checked={isEnabled}
       className={`${
-        enabled ? 'bg-zinc-900' : 'bg-gray-200'
-      } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
-      onChange={setEnabled}
+        isEnabled ? 'bg-zinc-900' : 'bg-gray-200'
+      } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-zinc-900 focus:ring-offset-2`}
+      onChange={toggle}
     >
       <span
         className={`${
-          enabled ? 'translate-x-6' : 'translate-x-1'
+          isEnabled ? 'translate-x-6' : 'translate-x-1'
         } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
       />
     </Switch>
