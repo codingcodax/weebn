8d5f2721c0592e9c4459fb7d120818ea43a40901
Alexis Guzman <alexis12989x@gmail.com>
2022-11-11 18:07:18 -0600

    feat(ui): use headleass ui instead of radix ui

--- components/ui/Switch.tsx
+++ components/ui/Switch.tsx
@@ -1,11 +1,29 @@
-import * as SwitchRadix from '@radix-ui/react-switch';
+'use client';
 
-const Switch = () => {
+import { Switch } from '@headlessui/react';
+import { FC } from 'react';
+
+interface MySwitchProps {
+  enabled: boolean;
+  setEnabled: () => void;
+}
+
+const MySwitch: FC<MySwitchProps> = ({ enabled, setEnabled }) => {
   return (
-    <SwitchRadix.Root>
-      <SwitchRadix.Thumb />
-    </SwitchRadix.Root>
+    <Switch
+      checked={enabled}
+      className={`${
+        enabled ? 'bg-zinc-900' : 'bg-gray-200'
+      } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+      onChange={setEnabled}
+    >
+      <span
+        className={`${
+          enabled ? 'translate-x-6' : 'translate-x-1'
+        } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
+      />
+    </Switch>
   );
 };
 
-export default Switch;
+export default MySwitch;
