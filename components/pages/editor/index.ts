54506d369b75d1c8114d7a9938ea2652521e98f6
Alexis Guzman <alexis12989x@gmail.com>
2022-11-14 12:55:13 -0600

    feat(editor): export theme switch component

--- components/pages/editor/index.ts
+++ components/pages/editor/index.ts
@@ -1,5 +1,6 @@
 export { default as LivePreview } from './LivePreview';
 export { default as OptionItem } from './OptionItem';
 export { default as SettingsPane } from './SettingsPane';
+export { default as ThemeSwitch } from './ThemeSwitch';
 export { default as Tooltip } from './Tooltip';
 export { default as WidgetConfig } from './WidgetConfig';
