89d00a2b933f842f874526252a87ecdaaed8dddf
Alexis Guzman <alexis12989x@gmail.com>
2022-11-14 17:25:12 -0600

    feat(editor): make page as entire src for iframe

--- components/pages/editor/LivePreview.tsx
+++ components/pages/editor/LivePreview.tsx
@@ -7,7 +7,7 @@ interface LivePreviewProps {
 const LivePreview: FC<LivePreviewProps> = ({ page }) => {
   return (
     <div className='h-[250px] w-[600px] resize overflow-auto rounded-md border-2 border-dashed border-zinc-500'>
-      <iframe height='100%' src={`http://localhost:3000${page}`} width='100%' />
+      <iframe height='100%' src={page} width='100%' />
     </div>
   );
 };
