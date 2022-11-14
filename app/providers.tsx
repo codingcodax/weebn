97fec2a3867f16f8c61b1bd29b5105c8ff274c90
Alexis Guzman <alexis12989x@gmail.com>
2022-11-14 10:47:56 -0600

    feat(providers): create providers and import theme provider

---
+++ app/providers.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { FC } from 'react';
+import { ThemeProvider } from 'next-themes';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  return <ThemeProvider attribute='class'>{children}</ThemeProvider>;
+};
+
+export default Providers;
