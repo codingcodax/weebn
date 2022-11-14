645d201011fe1e9ff7c3b978a130ad3ad3215d80
Alexis Guzman <alexis12989x@gmail.com>
2022-11-14 16:15:44 -0600

    feat(editor): use custom hook use dark mode

--- components/pages/editor/ThemeSwitch.tsx
+++ components/pages/editor/ThemeSwitch.tsx
@@ -1,18 +1,12 @@
 import { useTheme } from 'next-themes';
 
-import { useHasMounted } from '~/hooks/index';
+import { useDarkMode, useHasMounted } from '~/hooks/index';
 import { Skeleton, Switch } from '~/components/ui';
 
 const ThemeSwitch = () => {
   const hasMounted = useHasMounted();
   const { theme, resolvedTheme, setTheme } = useTheme();
-
-  const isDarkMode =
-    theme === 'dark' || resolvedTheme === 'dark' ? true : false;
-
-  const toggleTheme = () => {
-    isDarkMode ? setTheme('light') : setTheme('dark');
-  };
+  const { isDarkMode, toggleTheme } = useDarkMode();
 
   return (
     <div className='absolute bottom-0 right-0 rounded-tl-md bg-zinc-100 p-4 text-zinc-800 opacity-0 hover:opacity-100'>
