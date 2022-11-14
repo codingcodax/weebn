1890e84de1dbce9cd53d24a11ef7a44c4968ee12
Alexis Guzman <alexis12989x@gmail.com>
2022-11-14 12:00:12 -0600

    feat(ui): export skeleton component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Icons } from './Icons';
 export { default as Input } from './Input';
+export { default as Skeleton } from './Skeleton';
 export { default as Switch } from './Switch';
