1a0352fdcafacfc15a4fcc8216eb4cdb55bc5d52
Alexis Guzman <alexis12989x@gmail.com>
2022-11-15 13:08:51 -0600

    feat(layout): set max width to 6xl and center

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import { Footer, Header } from '~/components/layout';
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='grid h-screen grid-rows-[auto_1fr_auto] gap-y-4'>
+    <div className='mx-auto grid h-screen max-w-6xl grid-rows-[auto_1fr_auto] gap-y-4'>
       <Header />
       {children}
       <Footer />
