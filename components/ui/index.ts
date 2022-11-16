4d28e8d103ac3a888d177c42fded923bc346657d
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 10:56:44 -0600

    feat(ui): export logo component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Icons } from './Icons';
 export { default as Input } from './Input';
+export { default as Logo } from './Logo';
 export { default as Skeleton } from './Skeleton';
 export { default as Switch } from './Switch';
