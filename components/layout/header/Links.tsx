8351695fc6f2be07eabffcb8e9aea958a7ad33ca
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 13:28:38 -0600

    feat(header): remove about and add how to use link

--- components/layout/header/Links.tsx
+++ components/layout/header/Links.tsx
@@ -3,8 +3,8 @@ import LinkItem from './LinkItem';
 const Links = () => {
   return (
     <ul className='flex space-x-4'>
-      <LinkItem href='/about'>About</LinkItem>
       <LinkItem href='#widgets'>Widgets</LinkItem>
+      <LinkItem href='/how-to-use'>How To Use</LinkItem>
     </ul>
   );
 };
