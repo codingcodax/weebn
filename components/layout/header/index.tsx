51cb1dfbb4c8b73a2c67e0246bddc7a0e0acc2bc
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 10:57:07 -0600

    feat(header): import logo component

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,9 +1,11 @@
+import { Logo } from '~/components/ui';
+
 import Links from './Links';
 
 const Header = () => {
   return (
     <header className='mx-auto flex w-full max-w-6xl justify-between px-4 pt-8'>
-      <p>Logo</p>
+      <Logo />
       <Links />
     </header>
   );
