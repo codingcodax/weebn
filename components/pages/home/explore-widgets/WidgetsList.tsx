9c1e3caad58428211898d1319d5499b4e25d37bb
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 16:08:13 -0600

    feat(widgets): disabled some widgets

--- components/pages/home/explore-widgets/WidgetsList.tsx
+++ components/pages/home/explore-widgets/WidgetsList.tsx
@@ -16,22 +16,25 @@ const widgetsItems = [
     image: ImageGalleryImage,
     title: 'Image Gallery',
     description: 'Images from Nekos',
+    disabled: true,
   },
   {
     href: '/daily-anime',
     image: DailyAnimeImage,
     title: 'Random Anime',
     description: 'Show a random anime to see',
+    disabled: true,
   },
 ];
 
 const WidgetsList = () => {
   return (
     <ul className='grid grid-cols-1 gap-4 min-[500px]:grid-cols-2 lg:grid-cols-3'>
-      {widgetsItems.map(({ href, image, title, description }) => (
+      {widgetsItems.map(({ href, image, title, description, disabled }) => (
         <WidgetItem
           key={title}
           description={description}
+          disabled={disabled}
           href={href}
           image={image}
           title={title}
