ce1fa9a048456c60372f451ee032a8b4b8f0ef72
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 16:02:51 -0600

    feat(links): add disabled option

--- components/layout/footer/LinkItem.tsx
+++ components/layout/footer/LinkItem.tsx
@@ -1,15 +1,22 @@
 import { FC, ReactNode } from 'react';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 interface LinkItemProps {
   href: string;
   children: ReactNode;
+  disabled?: boolean;
 }
 
-const LinkItem: FC<LinkItemProps> = ({ href, children }) => {
+const LinkItem: FC<LinkItemProps> = ({ href, children, disabled }) => {
   return (
     <li className='text-zinc-500 underline transition-colors duration-200 hover:text-zinc-600'>
-      <Link href={href}>{children}</Link>
+      <Link
+        className={clsx(disabled && 'cursor-not-allowed')}
+        href={disabled ? '#' : href}
+      >
+        {children}
+      </Link>
     </li>
   );
 };
--- components/layout/header/LinkItem.tsx
+++ components/layout/header/LinkItem.tsx
@@ -1,15 +1,22 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
 interface LinkItemProps {
   href: string;
   children: ReactNode;
+  disabled?: boolean;
 }
 
-const LinkItem: FC<LinkItemProps> = ({ href, children }) => {
+const LinkItem: FC<LinkItemProps> = ({ href, children, disabled }) => {
   return (
     <li className='text-zinc-500 transition-colors duration-200 hover:text-zinc-600'>
-      <Link href={href}>{children}</Link>
+      <Link
+        className={clsx(disabled && 'cursor-not-allowed')}
+        href={disabled ? '#' : href}
+      >
+        {children}
+      </Link>
     </li>
   );
 };
