1d9be6ee86c52026f2219b5f02e01a629738ab5c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 16:03:04 -0600

    feat(links): disabled links

--- components/layout/footer/Links.tsx
+++ components/layout/footer/Links.tsx
@@ -3,9 +3,15 @@ import LinkItem from './LinkItem';
 const Links = () => {
   return (
     <ul className='flex items-center space-x-8 text-center'>
-      <LinkItem href='/usage'>Usage Tutorial</LinkItem>
-      <LinkItem href='/contribute'>Contribute</LinkItem>
-      <LinkItem href='/relase-logs'>Release Logs</LinkItem>
+      <LinkItem disabled href='/usage'>
+        Usage Tutorial
+      </LinkItem>
+      <LinkItem disabled href='/contribute'>
+        Contribute
+      </LinkItem>
+      <LinkItem disabled href='/relase-logs'>
+        Release Logs
+      </LinkItem>
     </ul>
   );
 };
--- components/layout/header/Links.tsx
+++ components/layout/header/Links.tsx
@@ -3,8 +3,12 @@ import LinkItem from './LinkItem';
 const Links = () => {
   return (
     <ul className='flex space-x-4'>
-      <LinkItem href='#widgets'>Widgets</LinkItem>
-      <LinkItem href='/how-to-use'>How To Use</LinkItem>
+      <LinkItem disabled href='#widgets'>
+        Widgets
+      </LinkItem>
+      <LinkItem disabled href='/how-to-use'>
+        How To Use
+      </LinkItem>
     </ul>
   );
 };
