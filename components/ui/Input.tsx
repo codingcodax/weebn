7735c89a67e88b2a35a49f6f7c80f39313dbf896
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 16:36:24 -0600

    feat(ui): add classname props and use with cn util function

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react';
 
+import { cn } from 'utils';
+
 interface InputProps {
   value: string;
   disabled?: boolean;
-  id: string;
+  id?: string;
+  className?: string;
 }
 
-const Input: FC<InputProps> = ({ value, disabled, id }) => {
+const Input: FC<InputProps> = ({ value, disabled, id, className }) => {
   return (
     <input
-      className='w-full rounded-md bg-zinc-200 py-2 px-4'
+      className={cn('w-full rounded-md bg-zinc-200 py-2 px-4', className)}
       disabled={disabled}
       id={id}
       type='text'
