c6e850aaddc9a7f9b7c8f63f2254229871ca9767
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 13:28:59 -0600

    feat(footer): set margin top to 16

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -4,7 +4,7 @@ import Links from './Links';
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col items-center space-y-16 px-4 pb-8'>
+    <footer className='mt-16 flex flex-col items-center space-y-16 px-4 pb-8'>
       <Links />
       <p className='text-center'>
         &copy; 2022 — Handcrafted with 💜 by{' '}
