db7c49333d05e2d65e7a638ad8ed52f99f3d0f59
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 17:44:45 -0600

    feat(hooks): export use copy to clipboard hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useCopyToClipboard } from './useCopyToClipboard';
 export { default as useDarkMode } from './useDarkMode';
 export { default as useHasMounted } from './useHasMounted';
 export { default as useToggle } from './useToggle';
