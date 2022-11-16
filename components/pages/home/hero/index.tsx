68ddab5c3321968b1cf4091a62d5887ba45dca9a
Alexis Guzman <alexis12989x@gmail.com>
2022-11-16 13:28:10 -0600

    feat(home) add mockup image to hero

--- components/pages/home/hero/index.tsx
+++ components/pages/home/hero/index.tsx
@@ -1,16 +1,30 @@
+import Image from 'next/image';
 import Link from 'next/link';
+
+import EditorMockup from '~/public/images/editor-mockup.jpg';
 
 const Hero = () => {
   return (
-    <main className='h-[calc(100vh-1rem-3.5rem)] px-4'>
+    <main className='grid h-[calc(100vh-1rem-3.5rem)] px-4 md:grid-cols-2'>
       <div className='flex h-full flex-col justify-center'>
         <h1 className='text-5xl font-bold'>Weebn</h1>
         <h2 className='mb-4 text-xl text-zinc-700'>
-          Weeb widgets for Notion, free and easy to use
+          Level up your Notion with light weight weeb widgets fully cuztomizable
+          and ready to use.
         </h2>
         <Link className='primary-button inline-block' href='/#widgets'>
           Explore Widgets
         </Link>
+      </div>
+
+      <div className='md:flex md:items-center md:justify-end'>
+        <Image
+          alt='mockup of editor page customization quotes widget'
+          className='-translate-x-28 md:translate-x-0'
+          height={425}
+          src={EditorMockup}
+          width={464}
+        />
       </div>
     </main>
   );
