26b8db0369baa0b03e74d233aa94a825948de61c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-17 11:25:11 -0600

    feat(header): add title and description for quotes page

---
+++ app/widgets/quotes/head.tsx
@@ -0,0 +1,13 @@
+const Head = () => {
+  return (
+    <>
+      <title>weebn | configure quotes widget</title>
+      <meta
+        content='Quote widget for get anime quotes with dark mode option'
+        name='description'
+      />
+    </>
+  );
+};
+
+export default Head;
