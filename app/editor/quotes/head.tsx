d98e2e4000fd0eb48b709e008ecf1c4f89e34e2c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-17 12:24:18 -0600

    feat(seo): add next seo

---
+++ app/editor/quotes/head.tsx
@@ -0,0 +1,17 @@
+import { NextSeo } from 'next-seo';
+
+import SEO from 'next-seo.config';
+
+const Head = () => {
+  const updateMeta = {
+    ...SEO,
+    title: 'configure quotes widget',
+    description:
+      'configure a quote widget for get anime quotes with dark mode option',
+    titleTemplate: '%s',
+  };
+
+  return <NextSeo {...updateMeta} useAppDir={true} />;
+};
+
+export default Head;
