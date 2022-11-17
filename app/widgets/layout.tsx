2497e5976292e51f8338983a236f3fe9318f3668
Alexis Guzman <alexis12989x@gmail.com>
2022-11-17 17:12:57 -0600

    fix(widgets): add type for children in layout

--- app/widgets/layout.tsx
+++ app/widgets/layout.tsx
@@ -1,4 +1,4 @@
-const EditorLayout = ({ children }) => {
+const EditorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex h-screen flex-col items-center justify-center p-4 text-[#37352f] dark:bg-[#191919] dark:text-[#d3d3d3]'>
       {children}
