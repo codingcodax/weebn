592a5d3cfd31cc58e500252c0adbe91b791fa043
Alexis Guzman <alexis12989x@gmail.com>
2022-11-17 12:22:52 -0600

    feat(seo): replace with next seo

--- app/head.tsx
+++ app/head.tsx
@@ -1,38 +1,7 @@
+import { NextSeo } from 'next-seo';
+
+import SEO from 'next-seo.config';
+
 export default function Head() {
-  return (
-    <>
-      <title>weebn | weeb notion widgets</title>
-      <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta
-        content='Level up your Notion with light weight weeb widgets, fully customizable and ready to use.'
-        name='description'
-      />
-      <link href='/favicons/favicon.ico' rel='icon' />
-      <link
-        href='/favicons/apple-touch-icon.png'
-        rel='apple-touch-icon'
-        sizes='180x180'
-      />
-      <link
-        href='/favicons/favicon-32x32.png'
-        rel='icon'
-        sizes='32x32'
-        type='image/png'
-      />
-      <link
-        href='/favicons/favicon-16x16.png'
-        rel='icon'
-        sizes='16x16'
-        type='image/png'
-      />
-      <link href='/favicons/site.webmanifest' rel='manifest' />
-      <link
-        color='#27272a'
-        href='/favicons/safari-pinned-tab.svg'
-        rel='mask-icon'
-      />
-      <meta content='#27272a' name='msapplication-TileColor' />
-      <meta content='#ffffff' name='theme-color' />
-    </>
-  );
+  return <NextSeo {...SEO} useAppDir={true} />;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo';
+
 import '~/styles/globals.css';
 
 import Providers from './providers';
@@ -5,7 +7,41 @@ import Providers from './providers';
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <head />
+      <head>
+        <meta charSet='utf-8' />
+        <meta content='width=device-width, initial-scale=1' name='viewport' />
+        <link href='/favicons/favicon.ico' rel='icon' />
+        <link
+          href='/favicons/apple-touch-icon.png'
+          rel='apple-touch-icon'
+          sizes='180x180'
+        />
+        <link
+          href='/favicons/favicon-32x32.png'
+          rel='icon'
+          sizes='32x32'
+          type='image/png'
+        />
+        <link
+          href='/favicons/favicon-16x16.png'
+          rel='icon'
+          sizes='16x16'
+          type='image/png'
+        />
+        <link href='/favicons/site.webmanifest' rel='manifest' />
+        <link
+          color='#27272a'
+          href='/favicons/safari-pinned-tab.svg'
+          rel='mask-icon'
+        />
+        <meta content='#27272a' name='msapplication-TileColor' />
+        {/* <meta content='#ffffff' name='theme-color' /> */}
+        <NextSeo
+          themeColor='#27272a'
+          titleTemplate='%s | weebn'
+          useAppDir={true}
+        />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
