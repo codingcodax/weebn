3a16a1d9d473618b957efed11cc3f2f4c98d9bb4
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 16:55:32 -0600

    feat(widgets): setup image gallery page

--- app/widgets/image-gallery/page.tsx
+++ app/widgets/image-gallery/page.tsx
@@ -1,7 +1,41 @@
+'use client';
+
+import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
+
 const ImageGallery = () => {
+  const searchParams = useSearchParams();
+
+  const getSearchParams = (param: string) => {
+    const searchParam = searchParams.get(param);
+
+    return searchParam === 'true' ? true : false;
+  };
+
+  const showSource = getSearchParams('showSource');
+
+  const shimmer = (w: number, h: number, hex: string) => `
+<svg width="${w}" height="${h}" viewBox="0 0 ${w} ${h}" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="${w}" height="${h}" fill="${hex}"/>
+</svg>`;
+
+  const toBase64 = (str: string) =>
+    typeof window === 'undefined'
+      ? Buffer.from(str).toString('base64')
+      : window.btoa(str);
+
   return (
-    <div>
-      <p>Image gallery page</p>
+    <div className='relative flex h-screen w-screen flex-col items-center justify-center'>
+      <Image
+        alt='A female anime/manga character.'
+        blurDataURL={`data:image/svg+xml;base64,${toBase64(
+          shimmer(1601, 2048, '#e0cfd0')
+        )}`}
+        fill={true}
+        objectFit='contain'
+        placeholder='blur'
+        src='https://cdn.waifu.im/161.jpeg'
+      />
     </div>
   );
 };
