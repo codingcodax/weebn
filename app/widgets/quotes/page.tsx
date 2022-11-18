1fb0ad8d02d39d332983dee8375645f5d7cfd735
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 10:56:14 -0600

    feat(quotes): add quote api endpoint and skeleton

--- app/widgets/quotes/page.tsx
+++ app/widgets/quotes/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import useSWR from 'swr';
 
 import { ThemeSwitch } from '~/components/pages/editor';
+import { Skeleton } from '~/components/ui';
+
+import { fetcher } from 'lib';
 
 const Quotes = () => {
+  const { data } = useSWR('/api/quote', fetcher);
   const searchParams = useSearchParams();
 
   const getSearchParams = (param: string) => {
@@ -20,25 +25,66 @@ const Quotes = () => {
   return (
     <div className='flex h-screen flex-col items-center justify-center p-4'>
       {showThemeButton && <ThemeSwitch />}
-      <div className='max-w-lg space-y-6'>
-        <h1 className='text-xl italic'>
-          &quot;Listen to yourself whining and complaining like some sorry
-          little victim! You can whimper all day for all I care, you&apos;re
-          nothing but a coward!&quot;
-        </h1>
+      {data ? (
+        <div className='max-w-lg space-y-6'>
+          <h1 className='text-xl italic'>&quot;{data?.quote}&quot;</h1>
 
-        {(showAnimeCharacter || showAnimeTitle) && (
-          <div className='h-px bg-[#dfdedd] dark:bg-[#373737]' />
-        )}
+          {(showAnimeCharacter || showAnimeTitle) && (
+            <div className='h-px bg-[#dfdedd] dark:bg-[#373737]' />
+          )}
 
-        <h2>
-          {showAnimeCharacter && 'Naruto Uzumaki '}
-          {showAnimeCharacter && showAnimeTitle && <span>&mdash; </span>}
-          {showAnimeTitle && 'Naruto'}
-        </h2>
-      </div>
+          <h2>
+            {showAnimeCharacter && `${data?.character} `}
+            {showAnimeCharacter && showAnimeTitle && <span>&mdash; </span>}
+            {showAnimeTitle && `${data?.anime}`}
+          </h2>
+        </div>
+      ) : (
+        <Loading>
+          <Loading.Quote /> <Loading.Divider />{' '}
+          <Loading.Footer>
+            {showAnimeCharacter && <Loading.Character />}{' '}
+            {showAnimeCharacter && showAnimeTitle && <span>&mdash; </span>}
+            {showAnimeTitle && <Loading.Anime />}
+          </Loading.Footer>
+        </Loading>
+      )}
     </div>
   );
 };
+
+interface LoadingProps {
+  children: React.ReactNode;
+}
+
+const Loading = ({ children }: LoadingProps) => {
+  return <div className='w-[512px] space-y-6'>{children}</div>;
+};
+
+// eslint-disable-next-line react/display-name
+Loading.Quote = () => (
+  <div className='space-y-2'>
+    <Skeleton className='w-full' />
+    <Skeleton className='w-full' />
+    <Skeleton />
+  </div>
+);
+
+// eslint-disable-next-line react/display-name
+Loading.Divider = () => <div className='h-px bg-[#dfdedd] dark:bg-[#373737]' />;
+
+interface LoadingFooterProps {
+  children: React.ReactNode;
+}
+// eslint-disable-next-line react/display-name
+Loading.Footer = ({ children }: LoadingFooterProps) => (
+  <div className='flex justify-start space-x-2'>{children}</div>
+);
+
+// eslint-disable-next-line react/display-name
+Loading.Character = () => <Skeleton className='w-[120px]' />;
+
+// eslint-disable-next-line react/display-name
+Loading.Anime = () => <Skeleton className='w-[50px]' />;
 
 export default Quotes;
