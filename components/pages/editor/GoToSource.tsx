c3e841b400739c9c282c6ad86c8db1b5c66e4f1c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 17:38:34 -0600

    feat(editor): setup go to source component

--- components/pages/editor/GoToSource.tsx
+++ components/pages/editor/GoToSource.tsx
@@ -1,7 +1,22 @@
-const GoToSource = () => {
+import Link from 'next/link';
+
+import { Icons } from '~/components/ui';
+
+interface GoToSourceProps {
+  url: string;
+}
+
+const GoToSource = ({ url }: GoToSourceProps) => {
   return (
-    <div>
-      <p>Go To Source Component</p>
+    <div className='absolute bottom-0 right-0 rounded-tl-md bg-zinc-100 p-4 text-zinc-800 opacity-0 hover:opacity-100'>
+      <Link
+        className='flex flex-col items-center'
+        href={url}
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        Source <Icons.arrowRight />
+      </Link>
     </div>
   );
 };
