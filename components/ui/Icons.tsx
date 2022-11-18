16f2f99d54f79e000a09ed07f6d8c66e2b0648d4
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 17:38:22 -0600

    feat(ui): add arrow right icon

--- components/ui/Icons.tsx
+++ components/ui/Icons.tsx
@@ -1,9 +1,10 @@
-import { ChevronLeft, Copy, Check } from 'lucide-react';
+import { ChevronLeft, Copy, Check, ArrowRight } from 'lucide-react';
 
 const Icons = {
   chevronLeft: ChevronLeft,
   copy: Copy,
   check: Check,
+  arrowRight: ArrowRight,
 };
 
 export default Icons;
