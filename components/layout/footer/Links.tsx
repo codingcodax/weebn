6d337f1132ac11bab49ff21273e535838f7846e7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 11:53:51 -0600

    feat(footer): update contribute link

--- components/layout/footer/Links.tsx
+++ components/layout/footer/Links.tsx
@@ -3,7 +3,7 @@ import LinkItem from './LinkItem';
 const Links = () => {
   return (
     <ul className='flex items-center space-x-8 text-center'>
-      <LinkItem disabled href='/contribute'>
+      <LinkItem href='https://github.com/codingcodax/weebn/blob/main/CONTRIBUTING.md'>
         Contribute
       </LinkItem>
       <LinkItem disabled href='/relase-logs'>
