2fe131dc180f5f79653619ea4fdfc7a11405f101
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 11:54:02 -0600

    feat(footer): create year function

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link';
 
 import Links from './Links';
 
+const year = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className='mt-16 flex flex-col items-center space-y-16 px-4 pb-8'>
       <Links />
       <p className='text-center'>
-        &copy; 2022 — Handcrafted with 💜 by{' '}
+        &copy; {year} — Handcrafted with 💜 by{' '}
         <Link
           href='https://github.com/codingcodax/'
           rel='noopener noreferrer'
