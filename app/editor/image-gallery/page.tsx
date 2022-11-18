5ada121af45c8c9d9706652ec883d7bc694b1971
Alexis Guzman <alexis12989x@gmail.com>
2022-11-18 17:45:22 -0600

    feat(editor): set up image gallery editor page

--- app/editor/image-gallery/page.tsx
+++ app/editor/image-gallery/page.tsx
@@ -1,7 +1,68 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import {
+  LivePreview,
+  OptionItem,
+  SettingsPane,
+  Tooltip,
+  WidgetConfig,
+} from '~/components/pages/editor';
+import useToggle from '~/hooks/useToggle';
+
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_BASE_URL
+    : 'http://localhost:3000';
+const PATH = '/widgets/image-gallery?';
+const queries = 'showSource=true';
+
 const ImageGallery = () => {
+  const [widgetUrl, setWidgetUrl] = useState(`${BASE_URL}${PATH}${queries}`);
+  const [sourceIsEnable, toggleSourceIsEnabled] = useToggle(true);
+
+  useEffect(() => {
+    const updateWidget = () => {
+      const newQueries = `showSource=${sourceIsEnable}`;
+      const newWidgetUrl = `${BASE_URL}${PATH}${newQueries}`;
+
+      setWidgetUrl(newWidgetUrl);
+    };
+
+    updateWidget();
+  }, [sourceIsEnable]);
+
   return (
-    <div>
-      <p>Image gallery page</p>
+    <div className='grid h-screen grid-cols-3'>
+      <WidgetConfig>
+        <WidgetConfig.Nav />
+        <WidgetConfig.Title>Image Gallery</WidgetConfig.Title>
+
+        <SettingsPane>
+          <SettingsPane.Section>
+            <SettingsPane.Title>Settings</SettingsPane.Title>
+            <OptionItem>
+              <OptionItem.Name>Source</OptionItem.Name>
+              <OptionItem.Description>
+                Show source of the image
+              </OptionItem.Description>
+              <OptionItem.Switch
+                isEnabled={sourceIsEnable}
+                toggle={toggleSourceIsEnabled}
+              />
+            </OptionItem>
+          </SettingsPane.Section>
+        </SettingsPane>
+
+        <WidgetConfig.Footer widgetUrl={widgetUrl} />
+      </WidgetConfig>
+
+      <div className='col-span-2 flex flex-col items-center justify-center space-y-4'>
+        <Tooltip />
+
+        <LivePreview page={widgetUrl} />
+      </div>
     </div>
   );
 };
